4084d9155d97ddf72fe708c08243d2f8ce725f50
EDWINLLOJA <edwinalindorllojalucer@gmail.com>
2023-05-17 01:18:20 -0500

    bd

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
-        'password' => '',
-        'database' => '',
+        'username' => 'root',
+        'password' => 'ÑATOlu123@',
+        'database' => 'famisalud',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
